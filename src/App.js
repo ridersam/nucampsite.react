7c3e228e1be5cb2d930dc415638baa2fe2e7bdf9
Sam Rider <ridersam1023@gmail.com>
2023-08-08 18:19:57 -0500

    Pages

--- src/App.js
+++ src/App.js
@@ -1,15 +1,20 @@
-import { Container, Navbar, NavbarBrand } from 'reactstrap';
-import NucampLogo from'./app/assets/img/logo.png';
-import './App.css';
-import CampsitesList from './features/campsites/CampsitesList';
+import { Routes, Route} from 'react-router-dom';
+import ContactPage from './pages/ContactPage';
+import HomePage from './pages/HomePage';
+import CampsitesDirectoryPage from './pages/CampsitesDirectoryPage';
 import Header from './components/Header';
 import Footer from './components/Footer';
+import './App.css';
 
 function App() {
   return (
     <div className="App">
       <Header />
-      <CampsitesList/>
+        <Routes>
+          <Route path='/' element={<HomePage />} />
+          <Route path='contact' element={<ContactPage />} />
+          <Route path='directory' element={<CampsitesDirectoryPage />} />
+        </Routes>
       <Footer />
     </div>
   );
--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
+import {BrowserRouter} from 'react-router-dom';
 import './index.css';
 import App from './App';
 import { Provider } from 'react-redux';
@@ -10,14 +11,15 @@ import 'font-awesome/css/font-awesome.css';
 import 'typeface-lobster';
 import 'typeface-open-sans';
 
-
 const container = document.getElementById('root');
 const root = createRoot(container);
 
 root.render(
   <React.StrictMode>
     <Provider store={store}>
-      <App />
+      <BrowserRouter>     
+        <App />
+      </BrowserRouter> 
     </Provider>
   </React.StrictMode>
 );
