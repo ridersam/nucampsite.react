5b96cead2c24482feb4674ab107b3bd394e9e58a
Sam Rider <ridersam1023@gmail.com>
2023-08-29 21:26:57 -0500

    Async DisplayList

--- src/features/campsites/CampsitesList.js
+++ src/features/campsites/CampsitesList.js
@@ -2,21 +2,42 @@ import { useSelector } from 'react-redux';
 import { Col, Row } from 'reactstrap';
 import CampsiteCard from './CampsiteCard';
 import { selectAllCampsites } from './campsitesSlice';
+import Error from '../../components/Error';
+import Loading from '../../components/Loading';
 
 const CampsitesList = () => {
     const campsites = useSelector(selectAllCampsites);
     console.log('campsites:', campsites);
 
-    return(
+    const isLoading = useSelector((state) => state.campsites.isLoading);
+    const errMsg = useSelector((state) => state.campsites.errMsg);
+
+    if (isLoading) {
+        return (
+            <Row>
+                <Loading />
+            </Row>
+        );
+    }
+
+    if (errMsg) {
+        return(
+            <Row>
+                <Error errMsg={errMsg} />
+            </Row>
+        );
+    }
+
+    return (
         <Row className='ms-auto'>
             {campsites.map((campsite) => {
-                return(
+                return (
                     <Col
                         md='5'
                         className='m-4'
                         key={campsite.id}
                     >
-                    <CampsiteCard campsite={campsite} />
+                        <CampsiteCard campsite={campsite} />
                     </Col>
                 );
             })}
--- src/features/display/DisplayList.js
+++ src/features/display/DisplayList.js
@@ -1,9 +1,11 @@
-import { UseSelector, useSelector } from "react-redux";
+import { useSelector } from "react-redux";
 import { Col, Row } from "reactstrap";
 import AnimatedDisplayCard from "./AnimatedDisplayCard";
 import { selectFeaturedCampsite } from "../campsites/campsitesSlice";
 import { selectFeaturedPromotion } from "../promotions/promotionsSlice";
 import { selectFeaturedPartner } from "../partners/partnersSlice";
+import Error from "../../components/Error";
+import Loading from "../../components/Loading";
 
 const DisplayList = () => {
     const items = useSelector((state) => [
@@ -17,10 +19,17 @@ const DisplayList = () => {
     return (
         <Row>
             {items.map((item, idx) => {
+                const { featuredItem, isLoading, errMsg } = item;
+                if (isLoading) {
+                    return <Loading key={idx} />;
+                }
+                if (errMsg) {
+                    return <Error errMsg={errMsg} key={idx} />;
+                }
                 return (
-                    item && (
-                        <Col md className="m-1" key={idx}>
-                            <AnimatedDisplayCard item={item} />
+                    featuredItem && (
+                        <Col md className='m-1' key={idx}>
+                            <AnimatedDisplayCard item={featuredItem} />
                         </Col>
                     )
                 );
