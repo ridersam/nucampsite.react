7ca1e92047cbed0ecb333886efe9b112550b6bfa
Sam Rider <ridersam1023@gmail.com>
2023-08-30 17:40:08 -0500

    Final Workshop

--- src/App.js
+++ src/App.js
@@ -11,6 +11,7 @@ import AboutPage from './pages/AboutPage';
 import { fetchCampsites } from './features/campsites/campsitesSlice';
 import { fetchPartners } from './features/partners/partnersSlice';
 import { fetchPromotions } from './features/promotions/promotionsSlice';
+import { fetchComments } from './features/comments/commentsSlice';
 import './App.css';
 
 function App() {
@@ -19,7 +20,8 @@ function App() {
     useEffect(() => {
       dispatch(fetchCampsites());
       dispatch(fetchPartners());
-      dispatch(fetchPromotions())
+      dispatch(fetchPromotions());
+      dispatch(fetchComments())
     }, [dispatch]);
 
   return (
